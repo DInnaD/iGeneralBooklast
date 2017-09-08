0aea1faf0e014ff7c1b230927f78508fb6a7145e
DInnaD <DInnaD@users.noreply.github.com>
2017-09-08 22:38:47 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+localStorage.test="hello"; // here test is key and value is "hello".
+
+var CustomObject={};
+
+localStorage.object=JSON.stringify(CustomObject); store custom object 
+
+console.log(JSON.parse(localStorage.object)); //retrieve object and display
